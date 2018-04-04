a6d02af7a0ec702da87d4b47094e4fd09c62e49e
KedrAdissey <455514@mail.ru>
2018-04-04 18:39:10 +0700

    Замена фона, добавление рисунка, логин, пароль, переход, новая страница

--- app/src/main/java/com/example/kna/sbratomstasom/MainActivity.java
+++ app/src/main/java/com/example/kna/sbratomstasom/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.kna.sbratomstasom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText pass;
+    private Button btn
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addListenerOnButton() {
+        pass = findViewById(R.id.editText);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent("com.example.kna.sbratomstasom.Main2Activity");
+                        startActivity(intent);
+
+                    }
+                }
+        );
     }
 }
